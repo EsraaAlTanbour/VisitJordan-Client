61e89f99584f55e2b832ba33a64cf8f10b27878b
EsraaAlTanbour <esraa.tanbour@gmail.com>
2026-05-09 14:28:41 +0300

    addedlogin&signupforms

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import * as formik from "formik";
+import * as yup from "yup";
+import "../../css/Auth.css";
+import logo from "../../assets/visitjordan logo.png";
 
-const Login = () => {
+function Login() {
+  const { Formik } = formik;
+
+  const schema = yup.object().shape({
+    email: yup.string().email("Invalid email").required("Email is required"),
+    password: yup.string().required("Password is required"),
+  });
+
   return (
-    <div>Login</div>
-  )
+    <div className="auth-page">
+      <div className="auth-card">
+        <img src={logo} alt="Visit Jordan" className="auth-logo" />
+        <h2>Welcome Back!</h2>
+
+        <Formik
+          validationSchema={schema}
+          onSubmit={console.log}
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+        >
+          {({ handleSubmit, handleChange, values, touched, errors }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  isInvalid={touched.email && !!errors.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  isInvalid={touched.password && !!errors.password}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.password}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Button className="auth-btn" type="submit">
+                Login
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,9 +1,173 @@
-import React from 'react'
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import * as formik from "formik";
+import * as yup from "yup";
+import "../../css/Auth.css";
+import logo from "../../assets/visitjordan logo.png";
 
-const Signup = () => {
+function Signup() {
+  const { Formik } = formik;
+
+  const schema = yup.object().shape({
+    firstName: yup.string().required("First name is required"),
+    lastName: yup.string().required("Last name is required"),
+    username: yup.string().required("Username is required"),
+    phone: yup.string().required("Phone number is required"),
+    email: yup.string().email("Invalid email").required("Email is required"),
+    password: yup.string().min(6, "At least 6 characters").required("Password is required"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Passwords must match")
+      .required("Confirm password is required"),
+    isProvider: yup.boolean(),
+  });
+
   return (
-    <div>Signup</div>
-  )
+    <div>
+      <div className="auth-page">
+      <div className="auth-card signup-card">
+        <img src={logo} alt="Visit Jordan" className="auth-logo" />
+        <h2>Create Account</h2>
+
+        <Formik
+          validationSchema={schema}
+          onSubmit={console.log}
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            username: "",
+            phone: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+            isProvider: false,
+          }}
+        >
+          {({ handleSubmit, handleChange, values, touched, errors }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Row>
+                <Form.Group as={Col} md="6" className="mb-3">
+                  <Form.Control
+                    type="text"
+                    placeholder="First name"
+                    name="firstName"
+                    value={values.firstName}
+                    onChange={handleChange}
+                    isInvalid={touched.firstName && !!errors.firstName}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.firstName}
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group as={Col} md="6" className="mb-3">
+                  <Form.Control
+                    type="text"
+                    placeholder="Last name"
+                    name="lastName"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    isInvalid={touched.lastName && !!errors.lastName}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.lastName}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.username}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="tel"
+                  placeholder="Phone number"
+                  name="phone"
+                  value={values.phone}
+                  onChange={handleChange}
+                  isInvalid={touched.phone && !!errors.phone}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.phone}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  isInvalid={touched.email && !!errors.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  isInvalid={touched.password && !!errors.password}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.password}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm password"
+                  name="confirmPassword"
+                  value={values.confirmPassword}
+                  onChange={handleChange}
+                  isInvalid={touched.confirmPassword && !!errors.confirmPassword}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.confirmPassword}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Check
+                  type="checkbox"
+                  name="isProvider"
+                  label="I want to sign up as an experience provider"
+                  checked={values.isProvider}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Button className="auth-btn" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+    </div>
+    
+  );
 }
 
-export default Signup
+export default Signup;
